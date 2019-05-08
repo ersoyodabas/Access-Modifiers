eda92fc49dcc9d6b76bc73c8c6e82f7118500175
Ersoy Odabas <ersoyodabas@outlook.com>
2019-05-09 00:22:20 +0300

    commit 2

---
+++ AnotherAssembly/Product.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherAssembly
+{
+   public class Product
+    {
+        int Id { get; set; }
+        private string Name { get; set; }
+
+        public int Count { get; set; }
+        public static string Currency{ get; set; }
+        static string Picture { get; set; }
+
+        protected double Price { get; set; }
+        protected internal string Path { get; set; }
+        internal string Origin { get; set; }    
+
+    }
+    public class AnotherProduct
+    {
+        Product product = new Product();
+
+        void setValues()
+        {
+
+            //PRIVATE
+            product.Id = 1;
+            product.Name = 'A';
+
+            //PUBLIC
+            product.Count = 1;
+
+            //PUBLIC STATIC
+            product.Currency = "TL";
+
+            //STATIC
+            product.Picture = "a.jpg";
+
+            //PROTECTED
+            product.Price = 10;
+
+
+            //PROTECTED INTERNAL
+            product.Path = "/a/b/";
+
+            //INTERNAL
+            product.Origin = "TR";
+
+        }
+
+    }
+
+    public class AnotherProduct2 : Product
+    {
+
+        void setValues()
+        {
+
+            //PRIVATE
+            Id = 1;
+            Name = 'A';
+
+            //PUBLIC
+            Count = 1;
+
+            //PUBLIC STATIC
+            Currency = "TL";
+
+            //STATIC
+            Picture = "a.jpg";
+
+            //PROTECTED
+            Price = 10;
+
+            //PROTECTED INTERNAL
+            Path = "/a/b/";
+
+            //INTERNAL
+            Origin = "TR";
+
+        }
+        
+
+    }
+
+
+    public static class AnotherProduct3 
+    {
+        public static int Id { get; set; }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,94 @@
+﻿using AnotherAssembly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Product product = new Product();
+
+            //PRIVATE
+            product.Id = 1;
+            product.Name = 'A';
+
+            //PUBLIC
+            product.Count = 1;
+            
+            //PUBLIC STATIC
+            product.Currency = "TL";
+            
+            //STATIC
+            product.Picture = "a.jpg";
+
+            //PROTECTED
+            product.Price = 10;
+
+
+            //PROTECTED INTERNAL
+            product.Path = "/a/b/";
+
+            //INTERNAL
+            product.Origin = "TR";
+
+
+            //PUBLIC STATIC
+            AnotherProduct3.Id = 10;
+
+            //STATIC
+            AnotherProduct4.Id = 10;
+
+            //STATIC
+            AnotherProduct5.Id = 10;
+
+        }
+    }
+
+    class Testx : Product
+    {
+        void setValues()
+        {
+
+            //PRIVATE
+            Id = 1;
+            Name = 'A';
+
+            //PUBLIC
+            Count = 1;
+
+            //PUBLIC STATIC
+            Currency = "TL";
+
+            //STATIC
+            Picture = "a.jpg";
+
+            //PROTECTED
+            Price = 10;
+
+            //PROTECTED INTERNAL
+            Path = "/a/b/";
+
+            //INTERNAL
+            Origin = "TR";
+
+        }
+
+
+    }
+
+    static class AnotherProduct4
+    {
+        static int Id { get; set; }
+    }
+
+    static class AnotherProduct5
+    {
+        public static int Id { get; set; }
+    }
+}
